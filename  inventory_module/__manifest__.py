2a2ffd1b34fe9bf82c5a044099c296304f0542ca
Inover Olga <kuzma-inoverolga@mail.ru>
2025-11-17 09:52:23 +0300

    new

---
+++  inventory_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Inventory Integration',
+    'version': '1.0',
+    'summary': 'Integration with external inventory system', 
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/inventory_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
