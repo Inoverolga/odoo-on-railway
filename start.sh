69f86af34a25e29df8d4d47e2ee7c3580b438fa2
Inover Olga <kuzma-inoverolga@mail.ru>
2025-11-17 11:23:52 +0300

    Add security bypass parameter

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 /usr/bin/odoo --without-demo=all --db_host=$PGHOST --db_port=$PGPORT --db_user=$PGUSER --db_password=$PGPASSWORD --database=$PGDATABASE --init=base
+export PGDATABASE=railway
+python3 /usr/bin/odoo --without-demo=all --db_host=$PGHOST --db_port=$PGPORT --db_user=$PGUSER --db_password=$PGPASSWORD --database=$PGDATABASE
